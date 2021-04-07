c80c1843a7f482cc75c66a9b7ced6b369a683e1b
Ditlev Tøjner <ditlev@hackyourfuture.dk>
2021-04-07 16:19:51 +0200

    Updated the footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -8,6 +8,29 @@ import Link from '@material-ui/core/Link'
 
 // Styling
 const useStyles = makeStyles(theme => ({
+  callToAction:{
+    backgroundColor:"#fff",
+    padding: "10px",
+  },
+  callToActionContainer: {
+    backgroundColor:"#efefef",
+    margin: "10px",
+    padding: "20px",
+    minHeight: "220px"
+  },
+  callToActionLabel: {
+    margin: "0 0 20px 0",
+    border: "1px solid #ccc",
+    display: "inline-block",
+    padding: "0 10px",
+    fontWeight: 300,
+    color: "#666",
+  },
+  callToActionDescription: {
+    fontSize: "1rem",
+    fontFamily: '"Space Mono", monospace',
+    lineHeight: "1.5rem",
+  },
   footer: {
     backgroundColor: '#293a7d',
     paddingTop: '5px'
@@ -22,9 +45,41 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const mockCallToAction = [
+  {
+    label:"Donate",
+    description:"HackYourFuture is a not-for-profit organization and our services are entirely free for the students."
+  },
+  {
+    label:"Volunteer",
+    description:"Our teachers and mentors are professional web-developers that take share their knowledge and passion for technology with people who need it the most."
+  },
+  {
+    label:"Partnership",
+    description:"We're always happy for more help and smart partnerships."
+  },
+]
+
 export default () => {
   const classes = useStyles()
   return (
+    <>
+    <Grid
+      container
+      direction='row'
+      justify='center'
+      alignItems='baseline'
+      className={classes.callToAction}
+    >
+      {mockCallToAction.map((cta,i) => (
+        <Grid item lg={4} md={4} sm={12}>
+          <div className={classes.callToActionContainer}>
+            <h2 className={classes.callToActionLabel}>{cta.label}</h2>
+            <p className={classes.callToActionDescription}>{cta.description}</p>
+          </div>
+        </Grid>
+      ))}
+    </Grid>
     <Grid
       container
       direction='row'
@@ -32,8 +87,9 @@ export default () => {
       alignItems='center'
       className={classes.footer}
     >
+
       <Grid item lg={8} md={7} sm={12}>
-        <Typography align='center' className={classes.info}>
+        <Typography align='left' className={classes.info}>
           Foreningen HackYourFuture | CVR: 38533193 |{' '}
           <Link
             className={classes.link}
@@ -49,5 +105,6 @@ export default () => {
         <SocialIcons />
       </Grid>
     </Grid>
+    </>
   )
 }
