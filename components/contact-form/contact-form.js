5cad487165350a1c516186bf893cadf1dfc3a2bb
Ditlev Tøjner <ditlev@hackyourfuture.dk>
2021-04-07 16:20:01 +0200

    updated the contact form

--- components/contact-form/contact-form.js
+++ components/contact-form/contact-form.js
@@ -2,6 +2,28 @@ import React, { useState } from 'react'
 import Icon from '@mdi/react'
 import { mdiSend } from '@mdi/js'
 import styles from './contact-form.scss'
+import containerStyles from './contact-form-container.scss'
+import { useContentfulEntryId } from '../../contentful/contentful-hooks'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+export function ContactformContainer(props) {
+  const contactFormDetails = useContentfulEntryId("57BmNlPMn5pBZBBvPNIoLC").content
+  const contactTitle = contactFormDetails && contactFormDetails.title
+  const contactBody = contactFormDetails && contactFormDetails.bodyText
+  const contactCover = contactFormDetails && contactFormDetails.media.fields.file.url
+  console.log(contactCover)
+  return (
+    <div className="contact-form-container" style={{backgroundImage:`url("${contactCover}")`}}>
+      <style jsx>{containerStyles}</style>
+      <div></div>
+      <article>
+      <h2>{contactTitle}</h2>
+      <div>{contactBody && documentToReactComponents(contactBody)}</div>
+      {props.children}
+      </article>
+    </div>
+  )
+}
 
 export default ({ email }) => {
   const [inputEmail, setInputEmail] = useState('')
