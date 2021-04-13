1c96eb8c76eefffef28ef33a248be185ddb4d8ed
Ditlev Tøjner <ditlev@hackyourfuture.dk>
2021-04-13 15:59:13 +0200

    added link capabilities to splash quote heading

--- components/layouts/content/SplashQuote.js
+++ components/layouts/content/SplashQuote.js
@@ -1,10 +1,19 @@
 import styles from './SplashQuote.scss'
+import Link from 'next/link'
 export default function SplashQuote(props) {
   if (props.content) {
     return (
       <div className="splash-quote dark-theme-quote">
         <h3>{props.content}</h3>
-        <h4>{props.heading}</h4>
+        {
+          props.link
+          ? (
+            <Link href={props.link}><h4>{props.heading}</h4></Link>
+          )
+          : (
+            <h4>{props.heading}</h4>
+          )
+        }
         <style jsx>{styles}</style>
       </div>
     )
