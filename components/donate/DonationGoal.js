0fdcaa515638e63d5226e89c6a341e02055a9a52
Ditlev Tøjner <ditlev@hackyourfuture.dk>
2021-04-14 17:42:29 +0200

    updated copy in donation goal

--- components/donate/DonationGoal.js
+++ components/donate/DonationGoal.js
@@ -1,15 +1,22 @@
 import styles from './DonationGoal.scss'
 
 const currently = 23
-const GOAL = 200
+const GOAL = 300
 
 export default function DonationGoal(props) {
     const goalReached = Math.abs(parseInt(((currently)/GOAL) * 100))
     return (
         <div className="donation-goal">
             <style jsx>{styles}</style>
-            <h1>Help us become a charitable association</h1>
-            <p>We need to gather 200 one time donation of atleast 200 kr for the HyF Association</p>
+            {
+                props.label
+                ? (<h1>{props.label}</h1>)
+                : (
+                    <h1>Help us become a charitable association</h1>
+                )
+            }
+            <p>Becoming an official charitable association would allow us easier access to municipal locations and enable us to offer tax breaks on donations under 16.300 kr.</p>
+            <p className="small-text">We need to gather 300 one time donation of atleast 200 kr for the HyF Association</p>
             <div className="donation-bar">
                 <div className="donation-indicator" style={{width:goalReached+"%"}}>{currently}</div>
                 <h2>200 members</h2>
